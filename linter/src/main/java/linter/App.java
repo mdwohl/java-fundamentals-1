9d0daf17b5ae4364089da5664128d9c64a4980f8
Paul OBrien <PVOVideo@outlook.com>
2020-09-16 22:53:17 -0700

    save current state

--- linter/src/main/java/linter/App.java
+++ linter/src/main/java/linter/App.java
@@ -3,12 +3,40 @@
  */
 package linter;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class App {
-    public String getGreeting() {
-        return "Hello world.";
+    public static void main(String[] args) {
+        Scanner file;
+        try {
+            file = new Scanner(new File("src/main/resources/gates.js"));
+        } catch (FileNotFoundException e) {
+            System.out.println("We couldn't find that file.");
+            return;
+        }
+
+        int counter = 0;
+        String errorMessage = "";
+
+        while (file.hasNextLine()) {
+            counter++;
+            String currentString = file.nextLine();
+            if (currentString.isBlank()) {
+            } else {
+//              counter++;
+                char endChar = currentString.charAt(currentString.length() - 1);
+                if (endChar == ';' || endChar == '{' || endChar == '}' || currentString.contains("if") || currentString.contains("else")) {
+                } else {
+                    errorMessage += "Line " + counter + ": Missing semicolon.\n";
+                }
+            }
+        }
+        System.out.println(errorMessage);
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+//    public String sayHi(){
+//        return "Hi!";
+//    }
 }
